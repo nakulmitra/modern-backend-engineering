a9773ce1a5e882cc920911df64b477e2d4214167
Nakul Mitra <nakulmitra2114@gmail.com>
2026-08-09 17:20:35 +0530

    added instances code file

---
+++ projects/LoadBalancer/instances/instance-1/src/main/java/com/tutorial/JavaLoadBalancerInstance1Application.java
@@ -0,0 +1,13 @@
+package com.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLoadBalancerInstance1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLoadBalancerInstance1Application.class, args);
+	}
+
+}
---
+++ projects/LoadBalancer/instances/instance-2/src/main/java/com/tutorial/controller/Controller.java
@@ -0,0 +1,23 @@
+package com.tutorial.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+	
+	@Value("${instance.name}")
+	private String instanceName;
+	
+	@GetMapping(value = "/hello", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String hello() {
+		return "Response from " + instanceName;
+	}
+	
+	@GetMapping(value = "/hi", produces = MediaType.TEXT_PLAIN_VALUE)
+	public String hi() {
+		return "Response from hi method with instance no: " + instanceName;
+	}
+}
